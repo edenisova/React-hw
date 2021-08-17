375459ca279e40971f186f0590727c2ce2030f06
unknown <katee98@yandex.ru>
2021-08-17 14:24:36 +0300

    Footer changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -26,24 +26,26 @@ export default function Footer(props) {
           </div>
         </div>
         <FooterLinks links={linksFirst} styleName="columnMl69"/>
-        <FooterLinks links={linksSecond} />
-        <FooterLinks links={linksThird} />
+        <FooterLinks links={linksSecond} styleName="columnMl35"/>
+        <FooterLinks links={linksThird} styleName="columnMl45"/>
         <div className={`${styles.fastAccess} ${styles.columnMl65}`}>
+        <div class={`${styles.footerSection} ${styles.mb23}`}>Fast access</div>
           <button className={styles.fastAccess__button}>
             Questions?
             {/*<span class="icon-help-circle fast-access__button-icon"></span>*/}
           </button>
-          <button class={styles.fastAccess__button}>
+          <button className={styles.fastAccess__button}>
             Settings
-            {/*<span class="icon-settings fast-access__button-icon"></span>*/}
+            <span className={`${styles.iconSettings} ${styles.fastAccess__buttonIcon}`}></span>
           </button>
         </div>
-        <div class={`${styles.lang} ${styles.ml60}`}>
+        <div className={`${styles.lang} ${styles.ml60}`}>
+        <div className={`${styles.footerSection} ${styles.mb23}`}>Language</div>
           <select
             name="Language"
             id="linkedin-lang"
             size="1"
-            class={styles.langSelect}
+            className={styles.langSelect}
           >
             <option value="English">English</option>
             <option value="Russian">Russian</option>
--- src/components/FooterLinks/FooterLinks.js
+++ src/components/FooterLinks/FooterLinks.js
@@ -2,8 +2,10 @@ import styles from './FooterLinks.module.css';
 
 
 export default function FooterLinks(props) {
+    const className = styles[props.styleName];
+{/*<div className={`${styles.navLinks} ${props.styleName}`}>*/}
     return (
-        <div className={`${styles.navLinks} ${props.styleName}`}>
+        <div className={`${styles.navLinks} ${className}`}>
             {props.links.map(item => <a className={styles.navLinks__text} href="">{item}</a>)}
         </div>
     );
